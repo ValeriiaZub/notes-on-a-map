aeb7565b3133def99235b69c90a362029127d34c
Valeriia Zub <val.zubb@gmail.com>
2025-05-07 18:11:12 -0400

    its a vibe

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { useState } from 'react'
+import { cn } from '@/lib/utils'
+import { noteStyle } from '../notes/note'
 
 interface AuthFormProps {
   isSignup?: boolean
@@ -11,11 +13,26 @@ interface AuthFormProps {
   signup: (formData: FormData) => void
 }
 
+function Card({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      style={{ ...noteStyle, width: 'unset', height: 'unset', filter: 'hue-rotate(300deg)' }}
+      data-slot="card"
+      className={cn(
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+
 export function AuthForm({ login, signup, isSignup = false }: AuthFormProps) {
   const [isSignUp, setSignUp] = useState(isSignup)
   return (
     <Card className="w-full max-w-md mx-auto">
-      <CardHeader>
+      <CardHeader style={{ width: '100%', height: '100%' }}>
         <CardTitle>{isSignUp ? 'Create Account' : 'Sign In'}</CardTitle>
         <CardDescription>
           {isSignUp
